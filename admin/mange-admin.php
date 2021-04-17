7186bcceea9cd3d11245b65425f47c58e9095ba4
Cs-kh <69515037+Cs-kh@users.noreply.github.com>
2021-04-17 15:47:46 +0300

    Add files via upload

---
+++ admin/mange-admin.php
@@ -0,0 +1,56 @@
+<?php include('partials/menu.php'); ?>
+
+
+
+
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+           <h1 class="header">Manage Admin</h1>
+           <!-- botton to add admin -->
+           <a href="#" class="btn-primary">Admin</a>
+<table class="tbl-full">
+    <tr>
+        <th>S.N</th>
+        <th>Full Name</th>
+        <th>UserName</th>
+        <th>Actions</th>
+    </tr>
+    <tr>
+        <td>1. </td>
+        <td>KHalid Rahim</td>
+        <td>khalid</td>
+        <td>
+            Update Admin
+            Delete Admin
+            
+        </td>
+    </tr>
+    <tr>
+        <td>1. </td>
+        <td>KHalid Rahim</td>
+        <td>khalid</td>
+        <td>
+            Update Admin
+            Delete Admin
+            
+        </td>
+    </tr>
+    <tr>
+        <td>1. </td>
+        <td>KHalid Rahim</td>
+        <td>khalid</td>
+        <td>
+            Update Admin
+            Delete Admin
+            
+        </td>
+    </tr>
+</table>
+        </div>
+    </div>
+    <!-- Main Content Section Ends -->
+
+
+
+ <?php include('partials/footer.php'); ?>
